1fdc5d94d9efe5d927d29344ce4eb72f53528d71
SeitaHigashi <higashi110902@gmail.com>
2018-11-11 10:38:30 +0900

    [add] create, create message

--- lib/elix_create.ex
+++ lib/elix_create.ex
@@ -1,7 +1,11 @@
 defmodule ElixCoreCreate do
   def response_create([command | message], _msg) do
     case command do
+      "message" -> create_message(message)
       _ -> "It does not match the command"
     end
   end
+
+  def create_message([head | tail]), do: head <> " " <> create_message tail
+  def create_message([]), do: ""
 end
