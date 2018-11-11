ae67f8cb0bd5811b3da4f2b322bc897c895b230e
SeitaHigashi <higashi110902@gmail.com>
2018-11-12 07:58:19 +0900

    [add] create channel

---
+++ lib/Create/elix_create_channel.ex
@@ -0,0 +1,7 @@
+defmodule ElixCreateChannel do
+  def channel_create([channel_name | _option], msg) do
+    guild_id = ElixGet.response_get(["guild_id"], msg) |> String.to_integer
+    { succesful , _ } = Nostrum.Api.create_channel(guild_id, %{name: channel_name})
+    succesful |> Atom.to_string
+  end
+end
